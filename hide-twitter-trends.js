331561ec4d0a11ce3732ae8ee7b1b96798593db2
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-10-13 22:05:39 +0200

    Match trends widget more conservatively
    Look a bit closer at the DOM to try to rule out false positives, though
    I haven’t found any so far. (The old code would have matched links in
    the settings, which the new code probably won’t, but I actually can’t
    find a /settings/trends link in my settings – only a /settings/explore
    link showing the same panel.)

--- hide-twitter-trends.js
+++ hide-twitter-trends.js
@@ -1,12 +1,32 @@
 'use strict';
 
+/**
+ * Try to remove the Trends widget.
+ * We mainly identify it using the cogwheel link in the corner of the widget,
+ * then look at some of the neighboring DOM trying to reduce false positives.
+ * (The many class names on the elements don’t look stable,
+ * so we only match on element names, hoping they’re more reliable.)
+ */
 function removeTrends() {
     for (let trends of document.querySelectorAll('a[href="/settings/trends"]')) {
         if (trends.closest('article')) {
             // trends link in a tweet
             continue;
         }
+        trends = trends.closest('h2');
+        if (!trends) {
+            // trends link in settings? maybe?
+            continue;
+        }
         trends = trends.closest('section');
+        if (!trends) {
+            // (unclear if this can happen)
+            continue;
+        }
+        if (trends.firstElementChild.tagName !== 'H1') {
+            // (unclear if this can happen)
+            continue;
+        }
         let parent;
         while ((parent = trends.parentElement).childElementCount == 1)
             trends = parent;
